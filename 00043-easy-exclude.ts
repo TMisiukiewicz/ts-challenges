87a87eaf8236637cb46540c93e86b3329776251d
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2023-08-03 21:47:05 +0200

    more easy tasks

---
+++ 00043-easy-exclude.ts
@@ -0,0 +1,23 @@
+/*
+  43 - Exclude
+  -------
+  by Zheeeng (@zheeeng) #easy #built-in #union
+
+  ### Question
+
+  Implement the built-in `Exclude<T, U>`
+
+  > Exclude from `T` those types that are assignable to `U`
+
+  For example:
+
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+
+  > View on GitHub: https://tsch.js.org/43
+*/
+
+/* _____________ Your Code Here _____________ */
+//https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+type MyExclude<T, U> = T extends U ? never : T;
